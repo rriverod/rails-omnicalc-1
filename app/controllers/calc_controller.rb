4228e585fc2ae86a3048e37f2e820e092365485b
rriverod <rriverod@chicagobooth.edu>
2025-02-13 00:11:21 +0000

    square root

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,5 +1,9 @@
 class CalcController < ApplicationController
 
+  def home
+    render({:template => "/calc_templates/square_new"})
+  end
+  
   def square_new
     render({:template => "/calc_templates/square_new"})
   end
@@ -8,6 +12,17 @@ class CalcController < ApplicationController
     @user_num=params.fetch("number").to_f
     @num_squared=@user_num *@user_num
     render({:template => "/calc_templates/square_results"})
+  end
+
+  def square_root_new
+
+    render({:template => "/calc_templates/square_root_new"})
+  end
+  
+  def square_root_results    
+    @user_num=params.fetch("number").to_f
+    @num_squared_root=Math.sqrt(@user_num)
+    render({:template => "/calc_templates/square_root_results"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  
+
+get("/", {:controller => "calc", :action =>"home"})
 get("/square/new", {:controller=>"calc", :action=>"square_new"})
 get("/square/results", {:controller=>"calc", :action=>"square_results"})
+
+
+get("/square_root/new", {:controller=>"calc", :action=>"square_root_new"})
+get("/square_root/results", {:controller=>"calc", :action=>"square_root_results"})
+
 
 
 end
